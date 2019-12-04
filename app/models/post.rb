d679de7d4a87e01a6d408270697d96cb15720365
rachfuerst <rach.fuerst@gmail.com>
2019-12-04 10:08:59 -0600

    Fixing another merge conflict

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
-<<<<<<< HEAD
-  belongs_to :neighborhoods
-=======
   belongs_to :neighborhood
->>>>>>> collection-select-on-posts-form
   has_one_attached :image
 
  
