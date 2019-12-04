4c123eb529d6e6cf59e4d7744151829a8cefe685
rachfuerst <rach.fuerst@gmail.com>
2019-12-03 19:53:52 -0600

    Fixed collection_select on the posts form and created the #show page for Posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,15 +1,27 @@
 class PostsController < ApplicationController
     def show
         @post= Post.find(params[:id])
+        
+        neighborhood_id= @post.neighborhood_id
+        @neighborhood  = Neighborhood.find(neighborhood_id)
+        
+    
     end
 
     def new
+        @neighborhoods= Neighborhood.all
         @posts= Post.all
         @post= Post.new
     end
 
     def create
+        # We needed to add Neighborhood.all to make this available to the create action
+        @neighborhoods= Neighborhood.all
         @post= Post.new(post_params)
+        @user= current_user
+        @post= Post.new(post_params)
+        @post.user= current_user
+
 
         if @post.valid?
             @post.save
@@ -22,6 +34,6 @@ class PostsController < ApplicationController
     
     private
     def post_params
-        params.require(:post).permit(:title, :description, :neighborhood, :intersection)
+        params.require(:post).permit(:title, :description, :neighborhood_id, :intersection)
       end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,9 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  def neighborhood
+    neighborhood_id= self.neighborhood_id
+    # neighborhood  = Neighborhood.where(neighborhood_id)
+  end
 end
