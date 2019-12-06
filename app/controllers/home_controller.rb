e3d9ee8b928ce68fcc66a7ea7cd63a1bd7224a33
rachfuerst <rach.fuerst@gmail.com>
2019-12-06 08:47:13 -0600

    Routing non-logged-in users to /welcome

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,15 +2,13 @@ class HomeController < ApplicationController
   before_action :require_login 
 
   def index
-  
     @posts= Post.all
   end
 
   
   private 
   def require_login
-  
-    redirect_to login_path unless session.include? :user_id 
+    redirect_to welcome_path unless session.include? :user_id 
   end
  
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,7 @@ class SessionsController < ApplicationController
        session[:user_id] = @user.id
        redirect_to '/home'
     else
-       redirect_to '/login'
+       redirect_to '/welcome'
     end
   end
 
